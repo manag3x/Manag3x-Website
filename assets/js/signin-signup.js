51166c19ad8dff0d83c0e6d0e1b82ddbecc853aa
Kazeem <muiliquazeem@gmail.com>
2021-07-10 19:34:48 +0100

    Ilium added a few styling and responsiveness

--- assets/js/signin-signup.js
+++ assets/js/signin-signup.js
@@ -1,3 +1,5 @@
+// ANIMATION EFFECT
+
 const login_btn = document.querySelector('#login-button'), 
 signup_btn = document.querySelector('#signup-button'), 
 container = document.querySelector('.container');
@@ -9,3 +11,48 @@ signup_btn.addEventListener('click', ()=>{
 login_btn.addEventListener('click', ()=>{
     container.classList.remove('enter-signup');
 });
+
+
+
+
+// INPUT VALIDATIONS
+
+function validateLogIn() {
+    const userEmail = document.querySelector("#email"), 
+    userPassword = document.querySelector("#password")
+
+    if (userEmail.value.trim() == ""){
+        userEmail.style.border = "2px solid red";
+        return false;
+    } else if (userPassword.value.trim() == ""){
+        userEmail.style.border = "2px solid red";
+        userPassword.placeholder = "You haven't input any password Fam!"
+        return false;
+    } else if (userPassword.value.trim().length < 6) {
+        userPassword.style.border = "2px solid red";
+        userPassword.placeholder = "Hello! Your password is too weak for my liking";
+        return false
+    } else  {
+        return true;
+    }
+}
+
+function validateSignUp() {
+    const signupUserEmail = document.querySelector("#signup-email"),
+    signupUserPassword = document.querySelector("#signup-password"),
+    confirmPassword = document.querySelector("#signup-confirm-password")
+
+    if (signupUserEmail.value.trim() == "") {
+        signupUserEmail.style.border = "2px solid red";
+        return false;
+    } else if (signupUserPassword.value.trim() == "") {
+        signupUserPassword.style.border = "2px solid red";
+        return false;
+    } else if (confirmPassword.value.trim() == "") {
+        confirmPassword.style.border = "2px solid red";
+        return false;
+    } else {
+        return true;
+    }
+    
+}
