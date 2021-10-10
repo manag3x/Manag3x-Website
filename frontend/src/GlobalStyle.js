15bfcba0834af3ee47dee8c5950367eb2f242697
Uchechukwu Samuel Ottah <71130767+coderboy-exe@users.noreply.github.com>
2021-10-10 17:57:55 +0100

    frontend

---
+++ frontend/src/GlobalStyle.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --maxWidth: 1280px;
+        --white: #fff;
+        --black: #000000 //This is just a sample template
+        --lightBlue: #;
+        --midBlue: #;
+        --darkBlue: #;
+        --fontVeryLarge: 2.5rem;
+        --fontLarge: 1.5rem;
+        --fontMid: 1.2rem;
+        --fontSmall: 1rem;
+    }
+
+    * {
+        box-sizing: border-box;
+        font-family: 'Abel', sans-serif;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+
+        h1 {
+            font-size: 2rem;
+            font-weight:600;
+            color: var(--black);
+        }
+
+        h3 {
+            font-size: 1.1rem;
+            font-weight: 600;
+        }
+
+        p {
+            font-size: 1rem;
+            color: var(--black)
+        }
+    }
+
+`;
---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Wrapper, Content, Button } from './Sidebar.styles';
+
+const Sidebar = () => (
+    <Wrapper>
+        <Content>
+            <Button>
+
+            </Button>
+        </Content>
+    </Wrapper>
+
+)
+export default Sidebar;
